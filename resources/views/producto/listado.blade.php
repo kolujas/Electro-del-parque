48813aed6b694b19f8170e04e9015c925db813ff
JuanCruzAGB <juancarmentia@gmail.com>
2019-07-23 15:14:17 -0300

    Oxisensores correccion en el nombre

--- resources/views/producto/listado.blade.php
+++ resources/views/producto/listado.blade.php
@@ -127,5 +127,5 @@
 @endsection
 
 @section('js')
-    <script type="text/javascript" src="js/web/listado.js"></script>
+    <script type="text/javascript" src="js/producto/listado.js"></script>
 @endsection
